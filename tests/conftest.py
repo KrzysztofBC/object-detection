d2dfcfd2d932e715d8b9179413ff43b8c1637f5c
Krzysztof Bork-Ceszlak <ceszlak.krzysztof@gmail.com>
2021-07-27 00:36:06 +0200

    corrected paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,17 @@
 import pytest
 from pathlib import Path
 
+
 @pytest.fixture(scope="session")
 def samples_dir():
-    return Path(__file__).absolute().parent / "samples"
+    return f"{str(Path(__file__).absolute().parent.parent)}\\samples\\"
+
 
 @pytest.fixture()
 def sample_image(samples_dir):
-    return samples_dir / 'vid_5_26720.jpg'
+    return samples_dir + 'vid_5_26720.jpg'
+
 
 @pytest.fixture()
 def coco_labels(samples_dir):
-    return samples_dir / 'labels.csv'
+    return samples_dir + 'labels.csv'
