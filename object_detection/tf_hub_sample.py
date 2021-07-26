c1d693b59e043703f8bea8b7f3468f3dd03f74d2
Krzysztof Bork-Ceszlak <ceszlak.krzysztof@gmail.com>
2021-07-27 00:36:57 +0200

    loading image and detecting cars

--- object_detection/tf_hub_sample.py
+++ object_detection/tf_hub_sample.py
@@ -28,3 +28,29 @@ class TfHubSampleDetector:
         self.det_source = f'https://tfhub.dev/tensorflow/efficientdet/d{self.det_type}/1'
         print(f"Downloading model from {self.det_source}")
         self.detector = hub.load(self.det_source)
+
+    def load_image(self, path_to_image):
+        """
+        Loading image by given path
+        :param path_to_image: place where image is stored
+        """
+
+        return cv2.imread(path_to_image)
+
+    def prepare_image(self, image, target_size):
+        """
+        Preparation for detection.
+        :param image: numpy array type of an image, 3 dims, HWC convention
+        :param target_size: tuple containing width and height of an input image to resize
+        """
+
+        # resize
+        image_resized = cv2.resize(image, target_size)
+        # convert bgr to rgb
+        image_rgb = cv2.cvtColor(image_resized, cv2.COLOR_BGR2RGB)
+        # creating tensor and convert to uint8
+        img_tensor = tf.convert_to_tensor(image_rgb, dtype=tf.uint8)
+        # adding extra dimension
+        img_tensor = tf.expand_dims(img_tensor, 0)
+
+        return img_tensor
