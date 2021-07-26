748ed9fc6da656697c701eda37afdd73992fd166
k.bork-ceszlak <k.bork-ceszlak@toucan-systems.pl>
2021-07-26 14:20:31 +0200

    tests configuration

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from pathlib import Path
+
+@pytest.fixture(scope="session")
+def samples_dir():
+    return Path(__file__).absolute().parent / "samples"
+
+@pytest.fixture()
+def sample_image(samples_dir):
+    return samples_dir / 'vid_5_26720.jpg'
+
+@pytest.fixture()
+def coco_labels(samples_dir):
+    return samples_dir / 'labels.csv'
