741e374de412d2169756700964afaa25c1572fd9
KrzysztofBC <ceszlak.krzysztof@gmail.com>
2021-08-22 13:43:18 +0200

    add coco labels path

--- tests/test_car_det.py
+++ tests/test_car_det.py
@@ -3,13 +3,13 @@ from object_detection import tf_hub_sample
 
 
 def test_loading_model_from_tf_hub():
-	tf_hub_det = tf_hub_sample.TfHubSampleDetector()
+	tf_hub_det = tf_hub_sample.TfHubSampleDetector(coco_labels_path='../samples/labels.csv')
 	output = tf_hub_det.detector(tf.ones((1, 256, 256, 3), tf.uint8))
 	assert output["detection_anchor_indices"].numpy().shape == (1, 100)
 
 
 def test_cars_detection(sample_image):
-	tf_hub_det = tf_hub_sample.TfHubSampleDetector()
+	tf_hub_det = tf_hub_sample.TfHubSampleDetector(coco_labels_path='../samples/labels.csv')
 	image = tf_hub_det.load_image(sample_image)
 	image_tensor = tf_hub_det.prepare_image(image, (1024, 1024))
 	output = tf_hub_det.detector(image_tensor)
