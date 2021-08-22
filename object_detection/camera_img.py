8f4c02e1c7d82320144b839abe4344cb869fc317
k.bork-ceszlak <k.bork-ceszlak@toucan-systems.pl>
2021-08-22 10:02:24 +0200

    camera image processing

---
+++ object_detection/camera_img.py
@@ -0,0 +1,25 @@
+"""
+using builtin camera to capture image and process it
+"""
+
+
+import cv2
+#import numpy as np
+#import tensorflow as tf
+#from object_detection import tf_hub_sample
+
+
+def process_img_from_camera():
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        success, img = cap.read()
+        if success:
+            img = cv2.flip(img, 1)
+            cv2.imshow("output", img)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+
+if __name__ == '__main__':
+    process_img_from_camera()
