bb4c96b92dc90d0815c46702e2395fe722ca0252
KrzysztofBC <ceszlak.krzysztof@gmail.com>
2021-08-22 13:42:33 +0200

    detect boxes with effdet

--- object_detection/tf_hub_sample.py
+++ object_detection/tf_hub_sample.py
@@ -6,17 +6,21 @@ We're gonna to find cars on a sample image.
 import cv2
 import tensorflow as tf
 import tensorflow_hub as hub
+import pandas as pd
+import numpy as np
 
 
 class TfHubSampleDetector:
     """
     Tensorflow Hub Detector sample class.
     """
-    def __init__(self, det_type=0):
+
+    def __init__(self, coco_labels_path, det_type=0):
         """
         :param det_type: type of an efficient detector 0-7
+        :param coco_labels_path: path to csv data of a coco labels
         """
-        if isinstance(det_type, int) and det_type >= 0 and det_type <= 7:
+        if isinstance(det_type, int) and 0 <= det_type <= 7:
             self.det_type = det_type
         else:
             print("Wrong type of model, choose int number within range 0-7.")
@@ -27,6 +31,12 @@ class TfHubSampleDetector:
         self.det_source = f'https://tfhub.dev/tensorflow/efficientdet/d{self.det_type}/1'
         print(f"Downloading model from {self.det_source}")
         self.detector = hub.load(self.det_source)
+
+        # reading coco labels
+        labels_coco = pd.read_csv(coco_labels_path, sep=';', index_col='ID')
+
+        # take only 2017 categories
+        self.labels_coco = np.array(labels_coco['OBJECT (2017 REL.)'])
 
     def load_image(self, path_to_image):
         """
@@ -41,6 +51,7 @@ class TfHubSampleDetector:
         Preparation for detection.
         :param image: numpy array type of an image, 3 dims, HWC convention
         :param target_size: tuple containing width and height of an input image to resize
+        :return img_tensor: image as 4D tensor
         """
 
         # resize
@@ -53,3 +64,32 @@ class TfHubSampleDetector:
         img_tensor = tf.expand_dims(img_tensor, 0)
 
         return img_tensor
+
+    def detect_boxes(self, img_tensor, score_threshold):
+        """
+        Detection with effdet.
+        :param img_tensor: ONE image as 4D tensor
+        :param score_threshold: threshold value above which boxes are chosen
+        :return (pred_scores, pred_boxes, pred_labels): detected objects, tuple
+        """
+
+        # detection
+        detector_output = self.detector(img_tensor)
+
+        # extract specific outputs
+        pred_boxes = detector_output['detection_boxes'].numpy()[0]
+        pred_scores = detector_output['detection_scores'].numpy()[0]
+        pred_labels = detector_output['detection_classes'].numpy().astype('int')[0]
+
+        # create chosen matrix based on score
+        chosen_matrix = pred_scores > score_threshold
+
+        # get data only for best scores
+        pred_scores = pred_scores[chosen_matrix]
+        pred_boxes = pred_boxes[chosen_matrix]
+        pred_labels = pred_labels[chosen_matrix]
+
+        # get classes names from coco
+        pred_labels = self.labels_coco[pred_labels - 1]
+
+        return pred_scores, pred_boxes, pred_labels
